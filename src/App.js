4a83322fe54d83951c0650ed9e9689f0bbfc5ba9
Vendel Toreki <vendel.toreki@liferay.com>
2022-04-28 21:00:38 +0200

    feat(step 1-3): Add Euros Input field

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import React from 'react';
+
+function Amount(props) {
+	const [val, setVal] = React.useState('')
+
+	function change(event) {
+		setVal(event.target.value)
+	}
+
+    return (
+	<div className={val < 0 ? 'negativeNumber' : ''}>
+		<label htmlFor="amount">{props.name}: </label>
+		<input id="amount" onChange={change} />
+	</div>
+	);
+}
 
 function App() {
   return (
     <div className="App">
+		<Amount name="Euros" />
     </div>
   );
 }
